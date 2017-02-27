963129e2af84bbb9214707a51eab5fbe6110f1f7
binsina <binsina9@gmail.com>
2017-02-27 14:51:17 -0500

    Add project files.

---
+++ Blog3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog3.Startup))]
+namespace Blog3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
